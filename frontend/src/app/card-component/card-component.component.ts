64797595706100a817a43a803d5bae09d22b73ce
tin_hong_wen <e03256506@u.nus.edu>
2022-03-19 16:09:26 +0800

    added new component to display results

--- frontend/src/app/card-component/card-component.component.ts
+++ frontend/src/app/card-component/card-component.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Injectable, Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { HttpClient, HttpHandler, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+import { HttpHandlingService } from '../http-handling.service';
 
 @Component({
   selector: 'app-card-component',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponentComponent implements OnInit {
   //no parameters unless got multiple uploads
-  constructor() { }
+  @Input() header = "";
+  @Input() buttonDesc  = "";
+  public  fileName: any;
+  private file: File;
+
+  @Output() receivedFileEvent = new EventEmitter<File>();
+
+  constructor(private dataService: HttpHandlingService) { }
 
   ngOnInit(): void {
   }
 
+  public onFileSelected($event: any) {
+    console.log("onFileSelected");
+    this.file = (<HTMLInputElement>event.target).files[0];
+    if(this.file) {
+      this.fileName = this.file.name
+    }
+    this.receivedFileEvent.emit(this.file);
+  }
 }
