1632d7bcea3de7b9cc90aa269b44f346e7b8bc40
tin_hong_wen <e03256506@u.nus.edu>
2022-03-19 16:06:20 +0800

    collateral changes

---
+++ frontend/src/app/result-table/result-table.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result-table',
+  templateUrl: './result-table.component.html',
+  styleUrls: ['./result-table.component.css']
+})
+export class ResultTableComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
